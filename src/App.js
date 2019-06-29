5e639ef81ccd61681db4bf553bb246d8a0b802e5
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-06-29 20:45:29 +0200

    Create new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => (
+    <div className="wrapper">
+    	<h1 className='mainHeader'>Hello world</h1>
+		<h2 className='secondaryHeader'>Hello eduweb</h2>
     </div>
   );
-}
+
 
 export default App;
