a2461877a37bc8aa21a73628fc1b57bdefddd362
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-06-29 22:29:42 +0200

    Create ListWrapper, ListItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
 
 
 const App = () => (
     <div className="wrapper">
-    	<h1 className='mainHeader'>Hello world</h1>
-		<h2 className='secondaryHeader'>Hello eduweb</h2>
+    	<ListWrapper />
     </div>
   );
 
